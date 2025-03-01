a3cac3109200f95145a7f1a820875eeeae915558
Manishhh10 <manishbhatt2121714@gmail.com>
2025-03-02 01:20:55 +0545

    project setup completed

---
+++ app/src/main/java/com/example/android/repository/UserRepository.kt
@@ -0,0 +1,29 @@
+package com.example.android.repository
+
+import android.adservices.adid.AdId
+import com.example.android.model.UserModel
+import com.google.firebase.auth.FirebaseUser
+
+interface UserRepository {
+//    {
+//        "success" : true,
+//        "meessage" : "Login success",
+//    }
+    fun login(email:String,password:String,callback:(Boolean,String) -> Unit)
+
+    fun signup(email: String,password: String,callback:(Boolean,String,String) -> Unit)
+
+    fun forgetPassword(email:String,callback: (Boolean,String) -> Unit)
+
+    fun addUserToDatabase(userId: String,userModel: UserModel,
+                          callback: (Boolean, String) -> Unit)
+
+    fun getCurrentUser() : FirebaseUser?
+
+    fun getUserFromDatabase(userId:String,callback: (UserModel?, Boolean, String)-> Unit)
+
+    fun logout(callback: (Boolean,String)->Unit)
+
+    fun editProfile(userId:String,data:MutableMap<String,Any>,
+                    callback: (Boolean,String) -> Unit)
+}
